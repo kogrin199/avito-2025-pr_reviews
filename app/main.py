488bd43e03298c40a60bdc78af453d6eb1f8424e
Grigoriy Kochm. <kogrin199@gmail.com>
2025-11-25 04:21:46 +0300

    [api] upd: registered api routers inside main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
+from app.api.pr import router as pr_router
+from app.api.review import router as review_router
+from app.api.team import router as team_router
+from app.api.user import router as user_router
+
 app = FastAPI(title="PR Reviewer Assignment Service", version="1.0.0")
 
-# Example healthcheck
+app.include_router(team_router)
+app.include_router(user_router)
+app.include_router(pr_router)
+app.include_router(review_router)
+
 @app.get("/health", tags=["Health"])
-def health():
+async def health():
     return {"status": "ok"}
